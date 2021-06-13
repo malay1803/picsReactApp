a6019bc646dd4154b8644eeca7ac5e460c553e11
malay1803 <18acharyamalay@gmail.com>
2021-06-13 19:13:05 +0530

    added grid to style the images and completed v2 implementation

--- src/Components/ImageList.js
+++ src/Components/ImageList.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import './ImageList.css'
+import ImageCard from './ImageCard'
 
 const ImageList = (props) => {
-    const images = props.images.map(({description,id,urls})=>{
-        return <img key={id} src={urls.regular} alt= {description} />
+    const images = props.images.map((image)=>{
+        return <ImageCard key={image.id} image={image} />
     })
     return (
-        <div>
+        <div className="image-list">
             {images}
         </div>
     )
